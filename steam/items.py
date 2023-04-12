ae3bf184b4ce2654a81eb7c2a84113f69ebc2d32
Rachel (Nakyung) Lee <52053044+rnklee@users.noreply.github.com>
2023-04-12 13:44:12 -0400

    Scraper modified
    Now collects num_reviews, num_positive_reviews, release_day/month/year, url for RPG games only and saves them in RPGproducts.json.

--- steam/items.py
+++ steam/items.py
@@ -9,12 +9,18 @@ from scrapy.loader import ItemLoader
 
 class ProductItem(scrapy.Item):
     name = scrapy.Field()
-    price = scrapy.Field()
     tags = scrapy.Field(
         output_processor = MapCompose(
             lambda x: x.replace('\r', '').replace('\n', '').replace('\t', '')
             )
         )
+    num_reviews = scrapy.Field()    
+    num_positive_reviews = scrapy.Field()
+    url = scrapy.Field()
+
+    release_month = scrapy.Field()
+    release_day = scrapy.Field()
+    release_year = scrapy.Field()
 
 
 class ProductLoader(ItemLoader):
--- steam/spiders/productspider.py
+++ steam/spiders/productspider.py
@@ -1,6 +1,8 @@
 import logging
-import re
 import pdb
+
+import re
+import datetime
 
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
@@ -14,7 +16,9 @@ logger = logging.getLogger(__name__)
 class ProductSpider(CrawlSpider):
 	name = 'productspider'
 	# category1=998 to search exclusively for games (not DLCs/soundtrackls/etc).
-	start_urls = ['https://store.steampowered.com/search/?category1=998&filter=topsellers']
+	# sort_by=Released_DESC to search exclusively for released games.
+	# tags=122 for RPGs.
+	start_urls = ['https://store.steampowered.com/search/?sort_by=Released_DESC&tags=122&category1=998&supportedlang=english']
 
 	allowed_domains = ['steampowered.com']
 	rules = [
@@ -39,16 +43,6 @@ class ProductSpider(CrawlSpider):
 		if '/agecheck/app' in response.url:
 			logger.debug(f"Form-type age check triggered for {response.url}.")
 
-			sessionID = response.xpath('//script[contains(.,"g_sessionID = ")]/text()').extract()[0]
-			sessionID = re.findall('g_sessionID = (.+?);', sessionID)[0]
-
-			formdata = {
-				'sessionID': sessionID,
-				'ageDay': '23',
-				'ageMonth': '10',
-				'ageYear': '1977'
-			}
-
 			app_number = re.findall('/[0-9]+/', response.url)[0]
 			app_number = app_number.replace('/', '')
 			url = 'https://store.steampowered.com/app/' + app_number
@@ -64,18 +58,16 @@ class ProductSpider(CrawlSpider):
 	def load_product(self, response):
 		loader = ProductLoader(item = ProductItem(), response = response)
 		loader.add_css('name', '.apphub_AppName ::text')
-
-		no_discount_price = response.css('div.game_purchase_price ::text').extract_first() # returns price when the game is not on sale and there's no discount_original_price.	
-		discount_price = response.css('div.discount_original_price ::text').extract_first() # despite its name should return the original price if the game's currently on sale.
-		price = no_discount_price or discount_price
-		#pdb.set_trace()
-
-		if not price:
-			logger.debug(f'No price found for {response.url}.')
-		if '$' not in price:
-			price = '$0.00' 
-		price = re.findall(r'\$[0-9]*\.[0-9]{2}', price)[0]
-		loader.add_value('price', price)
 		loader.add_css('tags', 'a.app_tag ::text')
+		loader.add_xpath('num_reviews', '//input[@id="review_summary_num_reviews"]/@value')
+		loader.add_xpath('num_positive_reviews', '//input[@id="review_summary_num_positive_reviews"]/@value')		
+		loader.add_value('url', response.url)
+
+		#pdb.set_trace()
+		release_date = response.xpath('//div[@class="release_date"]/div[@class="date"]/text()').extract_first()
+		release_date = datetime.datetime.strptime(release_date, '%b %d, %Y')
+		loader.add_value('release_month', release_date.month)
+		loader.add_value('release_day', release_date.day)
+		loader.add_value('release_year', release_date.year)		
 
 		return loader.load_item()
